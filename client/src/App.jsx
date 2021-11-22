876e40ddc3d273558b02fdd7ff6a25567e1730db
merlinjones10 <merlinjones10@gmail.com>
2021-11-22 03:01:41 +0000

    fetch changed to axios in front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Result from './components/Result';
 import { apiContext } from './context/ApiContext';
 import Layout from './components/layout/Layout';
 import CompareModal from './components/comparison/CompareModal';
+
 //
 function App() {
-  // Add data to list. I get true or false for the id of the item
+  // Copy of original data with 'compare' state added to Object. I get true or false for the id of the item when compare is clicked.
   const addComparisonItem = (id, state) => {
     setComparisonIds(
       comparisonIds.map((obj) => (obj.deal_id === id ? { ...obj, compare: state } : obj))
@@ -15,12 +17,19 @@ function App() {
   const [backendData, setBackendData] = useState({});
 
   useEffect(() => {
-    fetch('/api').then((response) =>
-      response.json().then((data) => {
-        setBackendData(data);
-        setComparisonIds(data.deals.map((elem) => ({ ...elem, compare: false })));
-      })
-    );
+    const fetchData = async () => {
+      const result = await axios('/api');
+      setBackendData(result.data);
+      setComparisonIds(result.data.deals.map((elem) => ({ ...elem, compare: false })));
+      console.log(result.data);
+    };
+    fetchData();
+    // fetch('/api').then((response) =>
+    //   response.json().then((data) => {
+    //     setBackendData(data);
+    //     setComparisonIds(data.deals.map((elem) => ({ ...elem, compare: false })));
+    //   })s
+    // );
   }, []);
 
   return (
