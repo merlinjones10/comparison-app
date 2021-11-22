b6bcd18233dc960caf5d7090f5918973ce43c3a6
merlinjones10 <78985319+merlinjones10@users.noreply.github.com>
2021-11-22 12:31:06 +0000

    bigfix with path names

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,25 +1,27 @@
-import * as React from 'react';
-import { useContext } from 'react';
-import { apiContext } from '../context/ApiContext';
-import DealCard from './individualDeal/DealCard';
-import { Container, LinearProgress, Skeleton } from '@mui/material';
+import * as React from "react"
+import { useContext } from "react"
+import { apiContext } from "../context/ApiContext"
+import DealCard from "./IndividualDeal/DealCard"
+import { Container, LinearProgress, Skeleton } from "@mui/material"
 
 const Result = ({ onCompare }) => {
-  const { deals } = useContext(apiContext);
+  const { deals } = useContext(apiContext)
   return (
     <>
       <section>
-        {typeof deals === 'undefined' ? (
+        {typeof deals === "undefined" ? (
           <Container>
             <LinearProgress />
             <Skeleton variant="rectangular" height={700} />
           </Container>
         ) : (
-          deals.map((deal) => <DealCard key={deal.deal_id} deal={deal} onCompare={onCompare} />)
+          deals.map((deal) => (
+            <DealCard key={deal.deal_id} deal={deal} onCompare={onCompare} />
+          ))
         )}
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Result;
+export default Result
